bc1a32be9a043f1828ef99434f70b4c0dcc612e6
Andrey Zhizhelev <andrey.zhizhelev@yandex.ru>
2020-12-03 19:51:17 +0300

    Update 1st task

--- 03.12.2020/Task_1.cpp
+++ 03.12.2020/Task_1.cpp
@@ -1,10 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int fileLength = 323;
+typedef string inputType;
 
-int counterOfTrees(string input[], int down, int right){
+int counterOfTrees(vector <string> input, int down, int right){
     int length = input[0].length();
+    int fileLength = input.size();
     int counter = 0, y = 0, x = 0;
     while(y < fileLength){
         if(input[y][x % length] == '#') counter ++;
@@ -16,9 +17,9 @@ int counterOfTrees(string input[], int down, int right){
 }
 
 int main(){
-    string input[fileLength];
-    for(int i = 0; i < fileLength; i++) cin >> input[i];
-    long long counter = 1;
-    counter *= counterOfTrees(input, 1, 3);
-    cout << counter;
+    freopen("Puzzle_Input.txt", "r", stdin);
+    vector <inputType> input;
+    inputType s;
+    while(cin >> s) input.push_back(s);
+    cout << counterOfTrees(input, 1, 3);
 }
